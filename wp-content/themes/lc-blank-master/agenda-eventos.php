84ff7e8844ff385173c4926705e9af643dfab851
Ameto11 <vinicius.giuliani@gmail.com>
2021-05-13 00:00:59 -0300

    fix: corrige tamanho da página enquanto os meses ainda não carregaram

--- wp-content/themes/lc-blank-master/agenda-eventos.php
+++ wp-content/themes/lc-blank-master/agenda-eventos.php
@@ -25,7 +25,7 @@ if (have_posts()) : while (have_posts()) : the_post();
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <div id="appagenda" class="container-eventos">
-            <div v-if="isLoading" style="color: gray">Carregando eventos...</div>
+            <div v-if="isLoading" style="color: gray; height: 2000px">Carregando eventos...</div>
             <!-- MESES POSTERIORES -->
             <div class="row">
                 <div class="col coluna-desktop">
