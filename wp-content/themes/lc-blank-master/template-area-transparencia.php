108a0ecb2b31ecb914f44f74d9568bd28ae2ff4e
Ameto11 <vinicius.giuliani@gmail.com>
2021-05-18 11:47:02 -0300

    feat: Adiciona funcionalidade 'mais vídeos' à área de transparência

--- wp-content/themes/lc-blank-master/template-area-transparencia.php
+++ wp-content/themes/lc-blank-master/template-area-transparencia.php
@@ -9,14 +9,15 @@ if (have_posts()) : while (have_posts()) : the_post();
 
         the_content();
 
+        $vuedev = 'vue.js';
         $vue = 'vue.min.js';
 
         $isLocalhost = get_site_url() == 'http://localhost/planodiretorsp';
 
         // include local
-        if ($localhosting) {
-            echo "<script type='text/javascript' src='./wp-content/themes/lc-blank-master/{$vue}'></script>";
-            echo "<script type='text/javascript' src='./wp-content/themes/lc-blank-master/axios.min.js'></script>";
+        if ($isLocalhost) {
+            echo "<script type='text/javascript' src='../wp-content/themes/lc-blank-master/{$vuedev}'></script>";
+            echo "<script type='text/javascript' src='../wp-content/themes/lc-blank-master/axios.min.js'></script>";
         } else {
             echo "<script type='text/javascript' src='../wp-content/themes/lc-blank-master/{$vue}'></script>";
             echo "<script type='text/javascript' src='../wp-content/themes/lc-blank-master/axios.min.js'></script>";
@@ -25,62 +26,25 @@ if (have_posts()) : while (have_posts()) : the_post();
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <div id="appvideos" class="container">
-            <div v-if="isLoading" style="color: gray">Carregando vídeos...</div>
-            <!-- VÍDEOS -->
-            <div class="row card-video" v-for="video in videos">
-                <div class="col-6">
-                    <!-- VIDEO EMBED -->
-                    <iframe width="500" height="280" :src="video.link.replace('watch?v=', 'embed/').replace('youtu.be/', 'youtube.com/embed/')" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
-                    </iframe>
+            <div v-if="carregando" style="color: gray">Carregando vídeos...</div>
+            <!-- VÍDEOS RECENTES -->
+            <div class="row card-video" v-for="video in videosRecentes">
+                <?php require('modulo-video.php'); ?>
+            </div>
+            <div v-if="!carregando" class="row mais-videos fechado" @click="alternaMaisVideos">
+                <div class="col decorativo">
                 </div>
-                <div class="col ficha-tecnica">
-                    <!-- FICHA DO VÍDEO -->
-                    <div class="row">
-                        <h2>{{video.titulo}}</h2>
-                    </div>
-                    <div class="row">
-                        <a v-if="logado" :href="'/evento?id=' + video.id">
-                            <div class="btn btn-primary">
-                                Editar evento
-                            </div>
-                        </a>
-                    </div>
-                    <div class="row">
-                        <span>{{formataData(video.data_evento)}}</span>
-                    </div>
-                    <div class="row video-info">
-                        <div class="col-1 p-0"><span>Evento:</span></div>
-                        <div class="col">
-                            <p>{{video.titulo}}</p>
-                        </div>
-                    </div>
-                    <div v-if="video.tema" class="row video-info">
-                        <div class="col-1 p-0"><span>Tema:</span></div>
-                        <div class="col">
-                            <p>{{video.tema}}</p>
-                        </div>
-                    </div>
-                    <div v-if="video.destaque" class="row video-info">
-                        <div class="col-1 p-0"><span>Fonte:</span></div>
-                        <div class="col">
-                            <p>{{video.destaque}}</p>
-                        </div>
-                    </div>
-                    <div class="row video-separador">
-                        ***
-                    </div>
-                    <div class="row">
-                        <p>Link do vídeo original: <a :href="video.link" target="_blank">{{video.link}}</a></p>
-                    </div>
-                    <div v-if="video.descricao" class="row">
-                        <p>{{video.descricao}}</p>
-                    </div>
-                    <div class="row">
-                        <span class="hashtag">#TransparênciaPDE</span>
+                <div class="col-2">
+                    <h2 class="">+ vídeos</h2>
+                </div>
+                <div class="col decorativo">
+                </div>
+                <div v-if="abreMaisVideos" class="container-mais-videos">
+                    <div class="row card-video" v-for="video in videos">
+                        <?php require('modulo-video.php'); ?>
                     </div>
                 </div>
             </div>
-        </div>
         </div>
 
         <script type="text/javascript">
@@ -90,8 +54,10 @@ if (have_posts()) : while (have_posts()) : the_post();
                 data: {
                     arrayMeses: ['JANEIRO', 'FEVEREIRO', 'MARÇO', 'ABRIL', 'MAIO', 'JUNHO', 'JULHO', 'AGOSTO', 'SETEMBRO', 'OUTUBRO', 'NOVEMBRO', 'DEZEMBRO'],
                     videos: [],
-                    isLoading: true,
-                    logado: false
+                    videosRecentes: [],
+                    carregando: true,
+                    abreMaisVideos: false,
+                    logado: false,
                 },
                 methods: {
                     formataData: function(dateStr) {
@@ -104,19 +70,38 @@ if (have_posts()) : while (have_posts()) : the_post();
                     checaLogin: function() {
                         if ("<?php echo is_user_logged_in() ?>")
                             this.logado = true
+                    },
+                    separaVideosRecentes: function() {
+                        if (this.videos.length > 0) {
+                            for (let quantidade = 3; quantidade > 0; quantidade--) {
+                                if (this.videos.length > 0) {
+                                    this.videosRecentes.push(this.videos.shift())
+                                } else {
+                                    return
+                                }
+                            }
+                        }
+                    },
+                    alternaMaisVideos: function() {
+                        document.getElementsByClassName("mais-videos")[0].classList.toggle("fechado")
+
+                        if (!this.abreMaisVideos) {
+                            this.abreMaisVideos = true;
+                        }
                     }
                 },
                 mounted() {
                     axios.get(listaVideos)
                         .then(response => {
                             this.videos = response.data.videos.reverse()
+                            this.separaVideosRecentes()
                             this.checaLogin()
                         })
                         .catch(error => {
                             console.error("ERRO AO OBTER VÍDEOS")
                             console.log(error)
                         })
-                        .finally(() => this.isLoading = false)
+                        .finally(() => this.carregando = false)
                 }
             });
         </script>
