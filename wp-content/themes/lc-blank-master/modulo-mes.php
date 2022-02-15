ae481e96d6b3881f0af6dded3df2f39285b03eed
vgiuliani11 <vinicius.giuliani@gmail.com>
2022-02-15 18:15:56 -0300

    Fix: eventos da agenda abrem ao clicar no título

--- wp-content/themes/lc-blank-master/modulo-mes.php
+++ wp-content/themes/lc-blank-master/modulo-mes.php
@@ -1,8 +1,8 @@
 <!-- Evento -->
 <div v-for="evento in mes.eventos" :key="evento.id" :class="evento.aberto ? 'row' : 'row fechado'">
     <div class="col-12">
-        <div class="row">
-            <div class="col-2">
+        <div class="row" @click="abreEvento(evento)">
+            <div class="col-auto">
                 <span class="linha-evento" :style="'color: '+mes.cor">{{evento.tipo}}</span>
             </div>
             <div class="col">
--- wp-content/themes/lc-blank-master/template-agenda.php
+++ wp-content/themes/lc-blank-master/template-agenda.php
@@ -15,7 +15,7 @@ if (have_posts()) : while (have_posts()) : the_post();
 ?>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-        <div id="appagenda" class="container-eventos">
+        <div id="appagenda" class="container container-eventos">
             <div v-if="carregando" style="color: gray; height: 2000px; font-size: 2em">Carregando conteúdo...</div>
             <!-- BOTÃO ADICIONAR EVENTO -->
             <div v-if="!carregando && logado" class="row justify-content-center mb-5">
@@ -331,6 +331,10 @@ if (have_posts()) : while (have_posts()) : the_post();
                     },
                     toggleEvento: function(evento) {
                         evento.aberto = !evento.aberto;
+                        this.$forceUpdate();
+                    },
+                    abreEvento: function(evento) {
+                        evento.aberto = true;
                         this.$forceUpdate();
                     },
                     checaDataProxima: function() {
