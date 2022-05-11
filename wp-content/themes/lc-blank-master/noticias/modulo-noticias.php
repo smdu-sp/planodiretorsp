ad9125b4f99ea17c2b79c9b6c03814e078e3e865
vgiuliani11 <vinicius.giuliani@gmail.com>
2022-05-11 14:14:59 -0300

    Organiza notícias para chamar via loop posteriormente

--- wp-content/themes/lc-blank-master/noticias/modulo-noticias.php
+++ wp-content/themes/lc-blank-master/noticias/modulo-noticias.php
@@ -2,13 +2,21 @@
 
 global $wpdb;
 $noticiasRaw = $wpdb->get_results("SELECT * FROM noticias ORDER BY id DESC;");
+
 $noticias = array_filter($noticiasRaw, function($obj) {
     return $obj->tipo == 'noticia' ? true : false;
 });
+
 $agenda = array_filter($noticiasRaw, function($obj) {
     return $obj->tipo == 'agenda' ? true : false;
 });
-$agenda = array_values($agenda);
+
+$maisNoticias = array_slice($noticias, 3);
+
+// Insere $agenda na quarta posição
+$noticias = array_slice($noticias, 0, 3) + $agenda;
+// Reordena as chaves do array
+$noticias = [...$noticias];
 
 ob_start();
 ?>
@@ -16,23 +24,23 @@ ob_start();
 <div id="noticias">
     <div class="container">
         <div class="row">
-            <div class="noticia">
+            <div class="<?= $noticias[0]->tipo ?>">
                 <img aria-hidden="true" src="<?= $noticias[0]->imagem ?>" alt="Capa da notícia" aria-hidden="true" />
                 <div class="link"><a href="<?= $noticias[0]->link ?>"><h3><?= $noticias[0]->titulo ?></h3></a></div>
             </div>
-            <div class="noticia">
+            <div class="<?= $noticias[1]->tipo ?>">
                 <img aria-hidden="true" src="<?= $noticias[1]->imagem ?>" alt="Capa da notícia" aria-hidden="true" />
                 <div class="link"><a href="<?= $noticias[1]->link ?>"><h3><?= $noticias[1]->titulo ?></h3></a></div>
             </div>
         </div>
         <div class="row">
-            <div class="noticia">
+            <div class="<?= $noticias[2]->tipo ?>">
                 <img src="<?= $noticias[2]->imagem ?>" alt="Capa da notícia" aria-hidden="true" />
                 <div class="link"><a href="<?= $noticias[2]->link ?>"><h3><?= $noticias[2]->titulo ?></h3></a></div>
             </div>
-            <div class="noticia agenda">
-                <img aria-hidden="true" src="<?= $agenda[0]->imagem ?>" alt="Capa da notícia" aria-hidden="true" />
-                <div class="link"><a href="<?= $agenda[0]->link ?>"><h3><?= $agenda[0]->titulo ?></h3></a></div>
+            <div class="<?= $noticias[3]->tipo ?>">
+                <img aria-hidden="true" src="<?= $noticias[3]->imagem ?>" alt="Capa da notícia" aria-hidden="true" />
+                <div class="link"><a href="<?= $noticias[3]->link ?>"><h3><?= $noticias[3]->titulo ?></h3></a></div>
             </div>
         </div>
     </div>
